5ea76a2ad400b15eba46482e69ec683db998d541
Muhammad Rauhan <113465334+muhammadrauhan@users.noreply.github.com>
2023-03-06 20:43:14 +0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,14 @@
+#Use an import statement to import statsmodels
+import statsmodels
+
+#Define a variable called bayes_age and set it equal to 4.0
+bayes_age = 4.0
+#Display the variable bayes_age
+print(bayes_age)
+#Define a variable called favorite_toy whose value is "Mr. Squeaky" also
+#Define a variable called owner whose value is 'DataCamp'
+favorite_toy = "Mr. Squeaky"
+#Import pandas
+import pandas as pd
+#Load the 'ransom.csv' into a DataFrame
+r = pd.read_csv('ransom.csv')
