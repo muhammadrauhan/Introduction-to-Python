e7d219625fcf5d27fb74648e4735390bb423369a
Muhammad Rauhan <113465334+muhammadrauhan@users.noreply.github.com>
2023-03-07 13:29:07 +0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,15 +1,48 @@
 #Use an import statement to import statsmodels
 import statsmodels
 
-#Define a variable called bayes_age and set it equal to 4.0
-bayes_age = 4.0
-#Display the variable bayes_age
-print(bayes_age)
-#Define a variable called favorite_toy whose value is "Mr. Squeaky" also
-#Define a variable called owner whose value is 'DataCamp'
-favorite_toy = "Mr. Squeaky"
-#Import pandas
-import pandas as pd
-#Load the 'ransom.csv' into a DataFrame
-r = pd.read_csv('ransom.csv')
+#MYSTERY OF BAYES' KIDNAPPING!
+#Filing a missing puppy report, which name is Bayes
+
+bayes_age = 4.0 #Define a variable called bayes_age
+print(bayes_age) #Display the variable bayes_age
+
+favorite_toy = "Mr. Squeaky" #His favorite toy
+
+import pandas as pd #Import pandas
+r = pd.read_csv('ransom.csv') #Load the 'ransom.csv' into a DataFrame
 print(r)
+#variable plate represents the observed license plate: the first three letters were FRQ, 
+#but the witness couldn't see the final 4 letters
+plate = 'FRQ****' #Use * to represent the missing four letters
+lookup_plate(plate)
+      #result 
+      #Fred Frequentist
+      #John W. Tukey
+      #Ronald Aylmer Fisher
+      #Karl Pearson
+      #Gertrude Cox
+      #Kirstine Smith
+#use color of the car to get smaller list      
+lookup_plate(plate, color='Green') #it also accepts a keyword arguments
+      #result Fred Frequentist
+            #Ronald Aylmer Fisher
+            #Gertrude Cox
+            #Kirstine Smith
+#some of them made suspicious purchases before the kidnapping!
+credit_records = pd.read_csv("credit_records.csv") #creditcard records are in csv
+credit_records.head()
+print(credit_records.head()) #display first five rows
+
+print(credit_records.info()) #to see how many rows are in this Dataframe
+#There are 104 rows in the Dataframe
+
+items = credit_records['item'] #selecting items from credit_records
+#OR
+items = credit_records.item #selecting item using dot notation
+
+print(mpr.info()) #Inspect the Dataframe mpr using info
+
+
+
+
